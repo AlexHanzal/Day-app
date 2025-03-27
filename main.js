44aa25406fabf590e6789be44cbcae547501316a
AlexHanzal <alexhanzalcz@gmail.com>
2025-03-27 20:08:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 
+
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow(index.html)
+})
+
+
+app.whenReady().then(() => {
+    createWindow()
+  })
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
